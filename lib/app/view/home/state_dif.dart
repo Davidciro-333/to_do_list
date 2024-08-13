5f550d01a227e64cdec31b4aa4228d5a9c6ee41b
Davidciro-333 <84923782+davidciro-333@users.noreply.github.com>
2024-08-12 23:42:52 -0500

    A example with Provider in state_dif.dart page, updating a Text Retrieving the email from the inherited widget _EmailTextField.

--- lib/app/view/home/state_dif.dart
+++ lib/app/view/home/state_dif.dart
@@ -1,75 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 /// A stateful widget that represents a form with an email input field and a submit button.
-class StateDif extends StatefulWidget {
+class StateDif extends StatelessWidget {
   /// Creates a [StateDif] widget.
   const StateDif({super.key});
-
-  /// Creates the mutable state for this widget.
-  @override
-  State<StateDif> createState() => _StateDifState();
-}
-
-/// The state for the [StateDif] widget.
-class _StateDifState extends State<StateDif> {
-  /// The email entered by the user.
-  String email = '';
 
   /// Builds the widget tree for the stateful widget.
   ///
   /// The [context] parameter is used to look up the widget tree.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-  // The main content of the page is wrapped in padding.
-  body: Padding(
-    padding: const EdgeInsets.all(40),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        // Padding around the email input field.
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20),
-          child: TextField(
-            // Updates the email variable when the text changes.
-            onChanged: (value) => email = value,
-            decoration: const InputDecoration(
-              // Fills the input field with white color.
-              filled: true,
-              fillColor: Colors.white,
-              // Adds an outline border with rounded corners.
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(16)),
-              ),
-              // Placeholder text for the input field.
-              hintText: 'Email',
-            ),
+    return ChangeNotifierProvider(
+      create: (_) => _EmailDisplay(),
+      child: const Scaffold(
+        // The main content of the page is wrapped in padding.
+        body: Padding(
+          padding: EdgeInsets.all(40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              /// Padding around the email input field.
+              _EmailTextField(),
+
+              /// A button to submit the form.
+              _SendButton(),
+
+              /// A text widget that displays the email written in the input field.
+              _EmailText(),
+            ],
           ),
         ),
-        // A button to submit the form.
-        ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            // Sets the minimum size of the button.
-            minimumSize: const Size(double.infinity, 50),
-            // Sets the background color of the button.
-            backgroundColor: Theme.of(context).colorScheme.primary,
-            // Adds rounded corners to the button.
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            // Sets the text style of the button.
-            textStyle: Theme.of(context).textTheme.bodyMedium?.copyWith(
+      ),
+    );
+  }
+}
+
+class _EmailText extends StatelessWidget {
+  const _EmailText();
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<_EmailDisplay>(builder: (_, emailDisplay, child) {
+      return Text(
+          'Email written in the text field will be displayed here ${emailDisplay.email}');
+    });
+    // Retrieves the email from the inherited widget.
+  }
+}
+
+class _SendButton extends StatelessWidget {
+  const _SendButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        // Sets the minimum size of the button.
+        minimumSize: const Size(double.infinity, 50),
+        // Sets the background color of the button.
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        // Adds rounded corners to the button.
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        // Sets the text style of the button.
+        textStyle: Theme.of(context).textTheme.bodyMedium?.copyWith(
               fontSize: 18,
               fontWeight: FontWeight.w700,
             ),
+      ),
+      // The text displayed on the button.
+      child: const Text('Send', style: TextStyle(color: Colors.white)),
+    );
+  }
+}
+
+class _EmailTextField extends StatelessWidget {
+  const _EmailTextField();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: TextField(
+        // Updates the email variable when the text changes.
+        onChanged: (value) => context.read<_EmailDisplay>().email = value,
+        decoration: const InputDecoration(
+          // Fills the input field with white color.
+          filled: true,
+          fillColor: Colors.white,
+          // Adds an outline border with rounded corners.
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(16)),
           ),
-          // The text displayed on the button.
-          child: const Text('Send', style: TextStyle(color: Colors.white)),
+          // Placeholder text for the input field.
+          hintText: 'Email',
         ),
-      ],
-    ),
-  ),
-);
+      ),
+    );
+  }
+}
+
+class _EmailDisplay extends ChangeNotifier {
+  String _email = '';
+
+  String get email => _email;
+
+  set email(String value) {
+    _email = value;
+    notifyListeners();
   }
 }
